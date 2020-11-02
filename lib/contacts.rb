af25e32868ac6517f7a3fdd1880c7d4dd7df0c94
Delia Constantino <dconstan92@gmail.com>
2020-11-02 16:59:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,24 +14,24 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    binding.pry
-  end
-end
-
-
-
 # def remove_strawberry(contacts)
 #   contacts.each do |person, contact_details_hash|
-#     if person == "Freddy Mercury"
-#       contact_details_hash.each do |attribute, value|
-#         if attribute == :favorite_ice_cream_flavors
-#           value.delete("strawberry")
-#         end
-#       end
-#     end
+#     binding.pry
 #   end
-#
-#
 # end
+
+
+
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete("strawberry")
+        end
+      end
+    end
+  end
+
+
+end
